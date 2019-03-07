ce438de6772a0ead0af6c11ece211868a05cd8df
Huy Luu <lmhuy_lmhuy@yahoo.com>
2019-03-07 01:06:34 -0800

    trying to fix port problem

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const port = 4003 || process.env.PORT;
 const app = express();
 
-app.use('',express.static('./public/index.html'));
+app.use(express.static('./public/index.html'));
 app.use('/:id', express.static('./public/index.html'));
 
 // app.listen(port, () => console.log(`Listening on port: ${port}`));
